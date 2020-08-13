085528c00e773817b41c5def9d77769e7222c99b
Maurice de Beijer <maurice.de.beijer@gmail.com>
2020-08-13 13:31:45 +0200

    Test title

--- src/App.test.js
+++ src/App.test.js
@@ -2,7 +2,7 @@ import React from "react";
 import { render } from "@testing-library/react";
 import App from "./App";
 
-test("renders learn react link", () => {
+test("renders the movies header", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/movies/i);
   expect(linkElement).toBeInTheDocument();
